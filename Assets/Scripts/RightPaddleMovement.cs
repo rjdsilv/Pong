d8cdd793e52a4690788cf1e16890e8288cca0e25
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-10-08 10:56:46 -0400

    Created the right paddle movement

--- Assets/Scripts/RightPaddleMovement.cs
+++ Assets/Scripts/RightPaddleMovement.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class LeftPaddleMovement : MonoBehaviour {
+public class RightPaddleMovement : MonoBehaviour {
     // The paddle as a rigid body.
     private Rigidbody2D rigidBody;
 
@@ -9,16 +11,18 @@ public class LeftPaddleMovement : MonoBehaviour {
      */
     public float speed = 0.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rigidBody = GetComponent<Rigidbody2D>();
-	}
-	
-	// FixedUpdate is called once per frame and is to be used with physics.
-	void FixedUpdate () {
-        float verticalMovement = Input.GetAxis("LeftPaddle");
+    }
+
+    // FixedUpdate is called once per frame and is to be used with physics.
+    void FixedUpdate()
+    {
+        float verticalMovement = Input.GetAxis("RightPaddle");
         Vector2 movement = new Vector2(0, verticalMovement);
         rigidBody.velocity = movement * speed;
         rigidBody.position = new Vector2(rigidBody.position.x, Mathf.Clamp(rigidBody.position.y, -4.40f, 4.40f));
-	}
+    }
 }
