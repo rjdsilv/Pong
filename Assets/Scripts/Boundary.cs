7810f6e433fa6bb409e57480a177ca55d97111b0
Rodrigo Januário da Silva <rjdsilv@gmail.com>
2017-10-08 11:13:23 -0400

    Generalized the movement.

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pong
+{
+    [Serializable]
+    public class Boundary
+    {
+        public float xMin;
+        public float xMax;
+        public float yMin;
+        public float yMax;
+    }
+}
---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Pong;
+
+/**
+ * Class      : PaddleMovement
+ * Description: This class will have the  paddle movement programed in order to make both paddles moving on the screen.
+ * Author     : Rodrigo Januario da Silva
+ * Version    : 1.0
+ */
+public class PaddleMovement : MonoBehaviour {
+    // The paddle as a rigid body.
+    private Rigidbody2D rigidBody2D;
+
+    /**
+     * Public variables in order to be set on the inspector
+     */
+    public float speed = 0.0f;
+    public Boundary boundary;
+    public string inputAxisName;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // FixedUpdate is called once per frame and is to be used with physics.
+    void FixedUpdate()
+    {
+        float verticalMovement = Input.GetAxis(inputAxisName);
+        Vector2 movement = new Vector2(0, verticalMovement);
+        rigidBody2D.velocity = movement * speed;
+        rigidBody2D.position = new Vector2(rigidBody2D.position.x, Mathf.Clamp(rigidBody2D.position.y, boundary.yMin, boundary.yMax));
+    }
+}
